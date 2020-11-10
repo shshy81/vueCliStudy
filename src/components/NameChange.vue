8a418bed7b231aadb71f4e2c55e173d99f4f97dc
shshy81 <shshy81@gmail.com>
2020-11-10 11:20:21 +0900

    slot - 부모요소에서 컴포넌트에 쓸 html 작성

--- src/components/NameChange.vue
+++ src/components/NameChange.vue
@@ -1,36 +1,21 @@
 <template>
     <div>
-        <p>{{ name }}</p>
-        <button @click="changeName">Click</button>
-        <hr>
-        <h2>stringProp : {{ stringProp }}</h2>
-        <h2 v-show="dynamicProp != null">dynamicProp : {{ dynamicProp }} </h2>
+        <p>header</p>
+        <!-- 컴포넌트 안에 별도로 html 코드를 넣고 싶을때 사용 -->
+        <slot name="header"></slot>
+        <p>body</p>
+        <slot name="body" :param="sendData"></slot>
+        <p>footer</p>
+        <slot :param="sendData"></slot>
     </div>
 </template>
 
 <script>
 export default {
-    props: {
-    // props: ['postStringProp'] // 주로 배열대신 Object 형태로 씀
-        stringProp: {
-            type: String,
-            required: true, // 해당 컴포넌트 사용시 무조건 prop전달해야함            
-        },
-        dynamicProp: {
-            type: String,
-            required: false,
-            default: 'default Title'
-        }    
-    } , 
     data() {
         return {
-            name: 'cho',
+            sendData: 'COMPONENT DATA',
         }
-    },
-    methods: {
-        changeName() {
-            this.name = 'CHO changed'
-        },
-    },
+    }
 }
 </script>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,7 +1,28 @@
 <template>
   <div class="about">
     <h1> {{ title }}</h1>
-    <NameChange stringProp="About 직접전달"/>
+    <NameChange> 
+        <!-- 부모요소에서 컴포넌트 선언 영역안에 작성한 html이 자식 컴포넌트의  slot 부분에 들어간다.
+        여러개의 slot은 자식컴포넌트에서 name으로 별칭을 지정하고 
+        부모요소에서 template v-slot:별칭 or #별칭 태그 안에 기재한 내용이
+        자식의 해당 별칭의 slot부분으로 들어간다. -->
+        <template v-slot:header>
+          Header slot Contents
+        </template>
+
+        <!-- 컴포넌트에서 data를 바인딩해서 보내면 부모에서 받아서 쓸수 있다. -->
+        <!-- 받아오는 변수명을 "변수명" 선언하고 사용할수 있고 data 명을 직접 적어 사용가능 -->
+        <template #body="propsName">
+          body slot Contents <br><br>
+          <p class="red">{{ propsName.param }}</p>
+        </template>
+
+        <!-- 별칭이 없는 slot은 #default로 매핑시킨다. -->
+        <template #default="{ param }">
+          default Contents <br><br>
+          <p class="red">{{ param }}</p>
+        </template>
+    </NameChange>
   </div>
 </template>
 
@@ -19,3 +40,7 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+.red {color : red}
+</style>
